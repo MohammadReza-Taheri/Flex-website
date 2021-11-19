a23b1574653547c786b85371b8036e7ec8a34a66
MohammadRezaTaheri-MRT <mrt_6913@yahoo.com>
2021-11-19 11:36:25 -0800

    Modified-temp

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-document.querySelector("section").addEventListener("click",(e)=>{
+document.querySelector("header").addEventListener("click",(e)=>{
     e.preventDefault();
-    let arrFromSection=Array.from(document.querySelector("section").children);
+    let arrFromSection=Array.from(document.querySelector("header").children);
     arrFromSection.forEach((item)=>{
         if(e.target.textContent===item.textContent){
             open(`${item.textContent}.html`,"index");
